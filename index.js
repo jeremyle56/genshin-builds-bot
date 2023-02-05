d93612127b623390aad1f29e40dad7b3b0538f26
Jeremy <jeremyle420@gmail.com>
2023-02-05 19:00:08 +1100

    Created slash commands

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-import dotenv from 'dotenv'
-dotenv.config()
-
-import { 
-    Client, 
-    GatewayIntentBits,
-} from 'discord.js';
+const fs = require("fs");
+const { Client, Collection, GatewayIntentBits } = require("discord.js");
+require("dotenv").config();
 
 const client = new Client({
     intents: [
@@ -14,22 +10,40 @@ const client = new Client({
     ],
 });
 
-const prefix = '!';
+// Add commands to client
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./commands').filter((file) => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.data.name, command);
+}
+
+// Handle commands
+client.on('interactionCreate', async (interaction) => {
+    if (!interaction.isCommand()) return;
+
+    const command = client.commands.get(interaction.commandName);
+
+    if(!command) return;
+
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        console.error(error);
+        await interaction.reply({
+            content: 'There was an error while executing this command!',
+            ephemeral: true,
+        });
+    }
+});
 
 client.once('ready', (c) => {
     console.log(`Ready! ${c.user.tag} is online`)
-})
+});
 
-client.on("messageCreate", async (message) => {
-    if (!message.content.startsWith(prefix) || message.author.bot) return;
-    const args = message.content.slice(prefix.length).split(/ +/);
-    const command = args.shift().toLowerCase();
-
-    if (command === 'ping') {
-        message.channel.send('pong!');
-    } else if (command === 'bruh') {
-        message.reply('bruh bruh');
-    }
+client.on('shardError', (error) => {
+    console.error('A websocket connection ecountered an error:', error);
 });
 
 client.login(process.env.DISCORD_TOKEN);
